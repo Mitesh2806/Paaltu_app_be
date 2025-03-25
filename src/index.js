c9cf28d384a5696fe60dd7bcee74719fe4e6d664
Mitesh2806 <miteshmaity_22519@aitpune.edu.in>
2025-03-25 21:25:48 +0530

    Cron job

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import express from 'express';
 import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
 import playdateRoutes from "./routes/playdateRoutes.js";
-import { connect } from 'mongoose';
+
 import { connectDB } from './lib/db.js';
 import cors from "cors";
+import job from './lib/cron.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
+job.start();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/playdates", playdateRoutes);
 app.get("/test", (req, res) => {
